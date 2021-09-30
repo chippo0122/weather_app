4ca7574605013fc3c2246e5547407b7dc56b353d
chippo0122 <79824220+chippo0122@users.noreply.github.com>
2021-09-30 18:18:58 +0800

    fix Request doesn't admit http API url
    instead use HTTPS, and try to fix video tag's control pannel still shows on iOS devices

--- src/api_config.js
+++ src/api_config.js
@@ -1,8 +1,8 @@
 const KEY = `cf6e9efe0171ea84a3911b65bdd27fd3`;
 const ONE_CALL_URL = `https://api.openweathermap.org/data/2.5/onecall`;
 //`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&lang=zh_tw&units=metric&appid=${KEY}`;
-const GEO_URL = `http://api.openweathermap.org/geo/1.0/direct`;
+const GEO_URL = `https://api.openweathermap.org/geo/1.0/direct`;
 //`http://api.openweathermap.org/geo/1.0/direct?q=${value}&limit=5&appid=${KEY}`;
-const CURRENT_WEATHER_URL = `http://api.openweathermap.org/data/2.5/weather`
+const CURRENT_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather`
 
 export {ONE_CALL_URL, GEO_URL, KEY, CURRENT_WEATHER_URL}
